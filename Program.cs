9680137b99c81273579d37f86169e85561122d39
Anna Barreto <86018831+sinivy@users.noreply.github.com>
2025-06-07 23:30:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using calculadora.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Conseno(30);
+calc.Tangente(30);
